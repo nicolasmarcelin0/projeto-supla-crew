15e5eb00c85b3c53a17e2f20ad4e56ba38aa0cb0
Nicolas Marcelino <nicolas.marcelino.mota08@aluno.ifce.edu.br>
2022-05-21 08:18:47 -0300

    ScrollView adicionado na tela de projetos

--- EcoInvest/EcoInvest/ProjetosView.swift
+++ EcoInvest/EcoInvest/ProjetosView.swift
@@ -20,94 +20,96 @@ struct ProjetosView: View {
     
     var body: some View {
         
-        
-        VStack (alignment: .center) {
+        ScrollView {
+            VStack (alignment: .center) {
+                
+                
+                Image("painel")
+                    .padding()
+                    .background(.gray)
+                    .frame(width: 340, height: 160)
+                    .cornerRadius(40)
+                    .shadow(radius: 5)
+                
+                
+                
+                Image("planeta")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                    .overlay{
+                        Circle().stroke(.white, lineWidth: 4)
+                    }
+                    .shadow(radius: 5)
+                    .padding( .top, -80)
+                
+                
+                
+                Text("Implementacao de Paineis Solares")
+                    .font(.headline)
+                    .fontWeight(.heavy)
+                    .frame(width: 325, height: 30)
+                    .background(Color.gray)
+                    .cornerRadius(15)
+                
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.")
+                    .font(.caption)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 370, height: 100)
+                
+                    .background(Color.init(red: 20/255, green: 105/255, blue: 105/255))
+                    .cornerRadius(15)
+                
+                Image("areaVerde")
+                    .padding()
+                    .background(.gray)
+                    .frame(width: 340, height: 160)
+                    .cornerRadius(40)
+                    .shadow(radius: 5)
+                
+                Image("arvore")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                    .overlay{
+                        Circle().stroke(.white, lineWidth: 4)
+                    }
+                    .shadow(radius: 5)
+                    .padding( .top, -80)
+                
+                
+                
+                Text("Implementacao de Áreas Verdes")
+                    .font(.headline)
+                    .fontWeight(.heavy)
+                    .padding()
+                    .frame(width: 325, height: 30)
+                    .background(Color.gray)
+                    .cornerRadius(15)
+                
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.")
+                    .font(.caption)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 370, height: 100)
+                    .background(Color.init(red: 20/255, green: 105/255, blue: 105/255))
+                    .cornerRadius(15)
+            }
             
-            
-            Image("painel")
-                .padding()
-                .background(.gray)
-                .frame(width: 340, height: 160)
-                .cornerRadius(40)
-                .shadow(radius: 5)
-            
-            
-            
-            Image("planeta")
-                .resizable()
-                .frame(width: 100, height: 100)
-                .clipShape(Circle())
-                .overlay{
-                    Circle().stroke(.white, lineWidth: 4)
-                }
-                .shadow(radius: 5)
-                .padding( .top, -80)
-            
-            
-            
-            Text("Implementacao de Paineis Solares")
-                .font(.headline)
-                .fontWeight(.heavy)
-                .frame(width: 325, height: 30)
-                 .background(Color.gray)
-                .cornerRadius(15)
-            
-            
-            Text("Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.")
-                .font(.caption)
-                .foregroundColor(Color.white)
-                .multilineTextAlignment(.center)
-                .multilineTextAlignment(.center)
-                .padding()
-                .frame(width: 370, height: 100)
- 
-                .background(Color.init(red: 20/255, green: 105/255, blue: 105/255))
-                .cornerRadius(15)
-            
-            Image("areaVerde")
-                .padding()
-                .background(.gray)
-                .frame(width: 340, height: 160)
-                .cornerRadius(40)
-                .shadow(radius: 5)
-            
-            Image("arvore")
-                .resizable()
-                .frame(width: 100, height: 100)
-                .clipShape(Circle())
-                .overlay{
-                    Circle().stroke(.white, lineWidth: 4)
-                }
-                .shadow(radius: 5)
-                .padding( .top, -80)
-            
-            
-            
-            Text("Implementacao de Áreas Verdes")
-                .font(.headline)
-                .fontWeight(.heavy)
-                .padding()
-               .frame(width: 325, height: 30)
-                .background(Color.gray)
-                .cornerRadius(15)
-            
-            
-            Text("Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.")
-                .font(.caption)
-                .foregroundColor(Color.white)
-                .multilineTextAlignment(.center)
-                .padding()
-                .frame(width: 370, height: 100)
-                .background(Color.init(red: 20/255, green: 105/255, blue: 105/255))
-                .cornerRadius(15)
         }
+    }
+    
+}
+
+struct ProjectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PerfilUsuarioView()
         
     }
 }
-    
-    struct ProjectsView_Previews: PreviewProvider {
-        static var previews: some View {
-            PerfilUsuarioView()
-            
-        }
-    }
