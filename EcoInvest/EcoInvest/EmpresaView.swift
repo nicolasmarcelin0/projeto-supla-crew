7e9df97a7ed876eeb598130b83be13fcd7fd9c42
Henrique Abn <102832927+HenriqueAbn@users.noreply.github.com>
2022-05-31 15:12:27 -0300

    alteracoes empresaview

--- EcoInvest/EcoInvest/EmpresaView.swift
+++ EcoInvest/EcoInvest/EmpresaView.swift
@@ -1,7 +1,131 @@
 import SwiftUI
 
 
+import SwiftUI
 
+struct GrowingButton2: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.title)
+            .multilineTextAlignment(.center)
+            .frame(width: 250, height: 60)
+            .background(Color("Green-System"))
+            .cornerRadius(15)
+            .shadow(radius: 2)
+            .foregroundColor(Color.black)
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
+struct EmpresaView: View {
+    @State var offset: CGFloat = 0
+    
+    var body: some View {
+        ZStack{
+        
+            Color(UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.00))
+                .edgesIgnoringSafeArea(.all)
+            
+        ScrollView(.vertical, showsIndicators: false,
+               content: {
+            VStack(spacing :15){
+                
+                GeometryReader{proxy -> AnyView in
+                    
+                    let minY = proxy.frame(in: .global).minY
+                    
+                    DispatchQueue.main.async{
+                        self.offset = minY
+                    }
+                    
+                    return AnyView(
+                        
+                        ZStack{
+                            Image("amazon_wallpaper")
+                                .resizable()
+                                .aspectRatio( contentMode: .fill)
+                                .frame(width:getRect().width,height: minY > 0 ? 180 + minY : nil, alignment: .center)
+                                .cornerRadius(0)
+                        }
+                            .frame(height: minY > 0 ? 180 + minY : nil)
+                        .offset(y: minY > 0 ? -minY : 0)
+                    )
+                }
+                .frame(height:180)
+                
+                VStack{
+                
+                    HStack{
+                        Image("Amazon")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                        .overlay{
+                            Circle().stroke(.white, lineWidth: 4)
+                        }
+                        .shadow(radius: 5)
+                        .padding( .top, -30)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .center, spacing: 10, content: {
+                            Text(empresa_padrao.nome)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.black)
+                            .padding(5)
+                            .cornerRadius(15)
+             
+                        })
+                    
+                    Spacer()
+                    ZStack{
+                        
+                    
+                    HStack {
+                        Image("delete-bin-6-fill")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .padding(.top, 25)
+                            .padding(.leading, -0)
+                            .shadow(radius: 5)
+         
+                        VStack {
+                            Text("Implementacao de Áreas Verdes")
+                                .font(.title2)
+                                .cornerRadius(15)
+                                .padding(.top, 15)
+                                .shadow(radius: 2)
+         
+                            HStack {
+                                Button("INVESTIR") {
+                                    print("...")
+                                }
+                                .buttonStyle(GrowingButton2())
+                            }
+                        }
+                        }
+                            
+                    }
+                }
+            }
+        })
+            .ignoresSafeArea(.all, edges: .top)
+    }
+    }
+}
+
+extension View{
+    func getRect()->CGRect{
+        return UIScreen.main.bounds
+    }
+}
+
+
+/*
 struct GrowingButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
@@ -59,12 +183,12 @@ struct EmpresaView: View {
                 .shadow(radius: 5)
  
             // Descrição da empresa do tipo investidor
-           /* Text(empresa_padrao.descricao)
+           /Text(empresa_padrao.descricao)
                 .font(.caption)
                 .padding()
                 .background(Color.init(red: 20/255, green: 155/255, blue: 105/255))
                 .cornerRadius(15)
-                .shadow(radius: 5)*/
+                .shadow(radius: 5)
             
             HStack {
                 Image("delete-bin-6-fill")
@@ -85,14 +209,14 @@ struct EmpresaView: View {
                         .shadow(radius: 5)
  
                     HStack {
-                       /* Text("Descrição do projeto ''Implementação de áreas verdes''")
+                       Text("Descrição do projeto ''Implementação de áreas verdes''")
                             .font(.caption)
                             .multilineTextAlignment(.center)
                             .frame(width: 210, height: 60)
                             .background(Color.init(red: 20/255, green: 115/255, blue: 105/255))
                             .cornerRadius(15)
                             .padding(.leading, 0)
-                            .shadow(radius: 5)*/
+                            .shadow(radius: 5)
  
                         
                         Button("Empresa 1") {
@@ -127,14 +251,14 @@ struct EmpresaView: View {
                         .shadow(radius: 5)
  
                     HStack {
-                       /* Text("Descrição do projeto")
+                        Text("Descrição do projeto")
                             .font(.caption)
                             .multilineTextAlignment(.center)
                             .frame(width: 210, height: 60)
                             .background(Color.init(red: 20/255, green: 115/255, blue: 105/255))
                             .cornerRadius(15)
                             .padding(.leading, 0)
-                            .shadow(radius: 5) */
+                            .shadow(radius: 5)
  
                         
                         Button("Empresa 2") {
@@ -148,7 +272,7 @@ struct EmpresaView: View {
                 }
                     
             }
-            /*
+            
             HStack {
                 Image("sun-foggy-fill")
                     .resizable()
@@ -186,13 +310,13 @@ struct EmpresaView: View {
                 }
                 
             }
-                */
+                
             }
         }
         .padding(.top, 0)
     }
         
-            /*
+            
             HStack(alignment: .center) {
                 Button {
                     print("you are not coming")
@@ -209,14 +333,14 @@ struct EmpresaView: View {
                 .padding(.leading, 40)
                 .padding(5)
         }
-        */
+        
     }
 }
-
+*/
 
 struct EmpresaView_Previews: PreviewProvider {
     static var previews: some View {
         EmpresaView()
     }
 }
-}
+
