37d72a72dbad1544a9e888b402b63b089b39cbec
Henrique Abn <102832927+HenriqueAbn@users.noreply.github.com>
2022-05-31 14:15:48 -0300

    Alteracoes na EmpresaView

---
+++ EcoInvest/EcoInvest/EmpresaView2.swift
@@ -0,0 +1,20 @@
+//
+//  EmpresaView2.swift
+//  EcoInvest
+//
+//  Created by aluno on 28/05/22.
+//
+
+import SwiftUI
+
+struct EmpresaView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmpresaView2_Previews: PreviewProvider {
+    static var previews: some View {
+        EmpresaView2()
+    }
+}
