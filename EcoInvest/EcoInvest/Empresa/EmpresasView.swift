716039436b30c627706a3236189bf3383d86a58e
Nicolas Marcelino <nicolas.marcelino.mota08@aluno.ifce.edu.br>
2022-06-14 08:38:33 -0300

    Upgrade

--- EcoInvest/EcoInvest/Empresa/EmpresasView.swift
+++ EcoInvest/EcoInvest/Empresa/EmpresasView.swift
@@ -28,46 +28,10 @@ struct EmpresasView: View {
                     
                 }
                 .navigationTitle(Text("Empresas"))
-                
             }
-            
-            
         }
-        
     }
 }
-
-
-
-//        VStack (alignment: .center) {
-//            ZStack(alignment: .leading){
-//                Rectangle()
-//                    .fill(Color(hue: 0.265, saturation: 0.647, brightness: 0.431))
-//                    .frame(width: .infinity, height: 100)
-//                HStack {
-//                    Image("logo")
-//                        .resizable()
-//                        .frame(width: 90, height: 90)
-//                        .padding()
-//
-//                    Text("Empresas")
-//                        .font(.largeTitle)
-//                        .fontWeight(.heavy)
-//                        .bold()
-//                        .foregroundColor(Color.white)
-//                        .padding()
-//                    }
-//                }
-//                SpecialButton(buttonText:"Pear",buttonImage:"Pear")
-//                SpecialButton(buttonText:"Amazon",buttonImage:"Amazon")
-//                SpecialButton(buttonText:"oBoticario",buttonImage:"oBoticario")
-//                SpecialButton(buttonText:"Natura",buttonImage:"Natura")
-//
-//            }
-
-
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
